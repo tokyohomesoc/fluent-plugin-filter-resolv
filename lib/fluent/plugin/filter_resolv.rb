81896c0fb8a443ce2188f5416165c2019e95969a
Simon Frostick <simon.frostick@zoo.ox.ac.uk>
2017-02-02 10:11:31 +0000

    renaming key_prefix to key_postfix postfix seems more fitting as the result is added after the key_name value

--- lib/fluent/plugin/filter_resolv.rb
+++ lib/fluent/plugin/filter_resolv.rb
@@ -5,7 +5,7 @@ module Fluent
     Plugin.register_filter('resolv', self)
 
     config_param :key_name,      :string, :default => 'host'
-    config_param :key_prefix,    :string, :default => 'fqdn'
+    config_param :key_postfix,    :string, :default => 'fqdn'
     config_param :remove_prefix, :string, :default => nil
     config_param :add_prefix,    :string, :default => nil
 
@@ -13,7 +13,7 @@ module Fluent
     def configure(conf)
       super
       @remove_prefix = Regexp.new("^#{Regexp.escape(remove_prefix)}\.?") unless conf['remove_prefix'].nil?
-      @key_prefix    = @key_name + "_" + @key_prefix
+      @key_postfix    = @key_name + "_" + @key_postfix
     end
 
     def filter_stream(tag, es)
@@ -22,7 +22,7 @@ module Fluent
       tag = (@add_prefix + '.' + tag) if @add_prefix
 
       es.each do |time,record|
-        record[@key_prefix] = Resolv.getname(record[@key_name]) rescue nil
+        record[@key_postfix] = Resolv.getname(record[@key_name]) rescue nil
         new_es.add(time, record)
       end
       return new_es
