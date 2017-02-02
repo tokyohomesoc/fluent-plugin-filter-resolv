dcb551b0d40bce8e1a74a5a394d9fd4656dc4f59
Simon Frostick <simon.frostick@zoo.ox.ac.uk>
2017-02-02 10:03:07 +0000

    reverse commit cb02c38, this now breaks in Release v0.14.12 - 2017/01/30, api shows filter_stream is correct http://docs.fluentd.org/v0.12/articles/plugin-development#filterstream-method

--- lib/fluent/plugin/filter_resolv.rb
+++ lib/fluent/plugin/filter_resolv.rb
@@ -16,7 +16,7 @@ module Fluent
       @key_prefix    = @key_name + "_" + @key_prefix
     end
 
-    def filter(tag, es)
+    def filter_stream(tag, es)
       new_es = MultiEventStream.new
       tag = tag.sub(@remove_prefix, '') if @remove_prefix
       tag = (@add_prefix + '.' + tag) if @add_prefix
